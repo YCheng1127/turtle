f2a966c856e27b5a1579ee334f0abc46fde7378e
YCheng <mgriffiba1127@gmail.com>
2018-05-28 01:42:40 +0800

    turtle without music and sometimes turtle stuck in the wall

--- js/main.js
+++ js/main.js
@@ -1,89 +1,168 @@
 /*
 locOrientation = screen.lockOrientation || screen.mozLockOrientation || screen.msLockOrientation || screen.orientation.lock;
 locOrientation("landscape");*/
+var begin=0;
+var rules=document.getElementById("rules");
+var start=document.getElementById("start");
+start.addEventListener("click",function(event){
+  event.stopPropagation();
+  start.style.display="none";
+  begin=1;
+  rules.style.display="none";
+},false)
+
+var run=requestAnimationFrame(rungame);
+function rungame(){//st
+ if(begin==1){  
 window.addEventListener("orientationchange",onOrientationchange,false);
 
 function onOrientationchange(){
   if(window.orientation === 180 || window.orientation === 0){
     phonestatus=1;//stand
-  /*
-  alert("hello i m standing!");
-  document.getElementById("all").style.transform="rotate(0deg)";
-  document.getElementById("all").style.width=window.innerHeight;
-  document.getElementById("all").style.height=window.innerWidth;*/
   }
   if(window.orientation === 90 || window.orientation === -90){
     phonestatus=2;//lie
   }
 }
-var phonestatus=1;
 
-var a=document.getElementById('al');
-var b=document.getElementById('be');
-var c=document.getElementById('ga');
+var phonestatus=1;//detect the phone status
+if(window.innerWidth<window.innerHeight)
+  phonestatus=1;
+else
+  phonestatus=2;
 
-var turtle=document.getElementById("turtleme");
+var score=document.getElementById("Score");//point
+var point=0;
+var gametime=document.getElementById("Time");
+var timeconstant=61;
+
+var turtle=document.getElementById("turtleme");//get turtle
+var turtleshocked=document.getElementById("turtleshocked");
 turtle.style.left="45%";
+turtleshocked.style.left="45%";
+turtle.style.bottom="0%";
+turtle.style.bottom="0%";
+
 var speed=0;
-var pos=43;
+var pos=43;//turtle position
 window.addEventListener('deviceorientation', function(event){
-    var alpha = event.alpha;
-    var beta = event.beta;
-    var gamma = event.gamma;
-    a.innerHTML=Math.round(alpha);
-    b.innerHTML=Math.round(beta);
-    c.innerHTML=Math.round(gamma);
     if(phonestatus==2)
       speed=event.beta*0.1;    
     else if(phonestatus==1)
       speed=event.gamma*0.1;
 
-    if(speed<0)    
+    if(speed<0){    
       turtle.style.transform="scaleX(-1)";
-    else
+      turtleshocked.style.transform="scaleX(-1)";
+    }
+    else{
       turtle.style.transform="scale(1)";
+      turtleshocked.style.transform="scale(1)";
+    }
 },false);
 
-requestAnimationFrame(Move);
+var move=requestAnimationFrame(Move);
+var grow=0;
+var growconstant=14;
+var growtime=0;
 var timecount=0;
+var on=0;//for jumping turtle
+var jumptime=0;// for jumping turtle
+
+var turtlebg=document.getElementById("turtle-bg");
+turtlebg.style.display="block";
+var r=0;
+var balllist=["flower","black","black","golden","bomb","bomb","black","black","golden","black","golden","bomb","golden","black","black","bomb"];
+var timedrop=[];//for floatingball
+var balls=[];//for floatingball
+var type=[];//for floatingball
+var timedroptest=0;
 function Move(){
   timecount++;
   console.log(timecount);
-  if(pos+speed<86&&pos+speed>0)
+  
+  gametime.innerHTML=Math.floor(timeconstant-timecount/60);
+
+  if(pos+speed<100-growconstant&&pos+speed>0)
     pos=pos+speed;
   turtle.style.left=pos+"%";
-/*  if(timecount%40 == 0)
-    floatblack(timecount/40);*/
-window.requestAnimationFrame(Move);
+  turtleshocked.style.left=pos+"%";
+  console.log("grow="+grow);
+  //grow the turtle when eat flower
+  if(grow==1){
+    growtime=growtime+16.66667;
+    growconstant=growconstant+0.055;
+    turtle.style.width=growconstant+"%";
+    turtleshocked.style.width=growconstant+"%";
+    if(growtime>=1500){
+    grow=0;
+    growtime=0;
+    }
+  }
+  //make balls randomly
+  if(timecount%40 == 0){
+    if(Math.floor((Math.random()*5)+1)!=2){
+      var draw=balllist[Math.floor(Math.random()*15)];
+      if(draw=="bomb"){
+        floatbomb(timecount/40);
+        balls[timecount/40]=["bomb",0];
+       }
+      else if(draw=="flower"){
+        floatflower(timecount/40);
+        balls[timecount/40]=["flower",0];
+      }
+      else if(draw=="black"){
+        floatblack(timecount/40);
+        balls[timecount/40]=["black",0];
+      }
+      else if(draw=="golden"){
+        floatgolden(timecount/40);
+        balls[timecount/40]=["golden",0];
+      }
+    }    
+  }
+  for(r=0;r<balls.length;r++){
+    if(document.getElementById("floatball"+r)){
+      if(balls[r][0]=="black")
+        dropstone(document.getElementById("floatball"+r),turtle,r);
+      else if(balls[r][0]=="golden")
+        dropstone(document.getElementById("floatball"+r),turtle,r);
+      else if(balls[r][0]=="flower")
+        dropflower(document.getElementById("floatball"+r),turtle,r);
+      else if(balls[r][0]=="bomb")
+        dropbomb(document.getElementById("floatball"+r),turtle,r);
+    }
+  }
+  
+  //jumpturtle
+  if(on==1){
+    jumptime=jumptime+16.6666667;
+    if(jumptime>600){
+      on=0;
+      jumptime=0;
+    }
+    turtle.style.bottom=jumptime*0.001*300-0.5*1000*jumptime*jumptime*0.001*0.001+"%";
+    turtleshocked.style.bottom=turtle.style.bottom;
+  }
+
+  score.innerHTML="&nbsp"+point;
+move=requestAnimationFrame(Move);
+
+  if(timecount/60>60)
+    cancelAnimationFrame(move);
 }
 
 
 
 //make the turtle jump
-var turtlebg=document.getElementById("turtle-bg");
-var on=0;
-turtlebg.addEventListener("touchstart", function(event){
+window.addEventListener("touchstart", function(event){
   if(on == 1)
     event.preventDefault();
   else{
   on=1;
-  var timeturtle=0;  
-  var turtlefly = setInterval(function(event){
-        if(timeturtle>600)
-          {on=0;clearInterval(turtlefly); }
-        else
-          timeturtle=timeturtle+20;
-        var time=timeturtle*0.001;
-        var height=300*time-0.5*1000*time*time;
-        if(height<0)
-          height=0;
-        
-        turtle.style.bottom=height+"%";        
-    }, 20)
-
     }
 },false);
-
+begin=0;
 
 //make a blacktomb
 var floatblack=function(k){
@@ -93,6 +172,7 @@ var floatblack=function(k){
   pic.setAttribute("id","floatball"+k);
   pic.style.position="absolute";
   pic.style.left=Math.floor((Math.random()*92)+1)+"%";
+  pic.style.transform="rotate("+Math.floor((Math.random()*360))+"deg)";
   turtlebg.appendChild(pic);
 }
 
@@ -119,7 +199,6 @@ var floatflower=function(k){
   pic.style.left=Math.floor((Math.random()*93)+1)+"%";
   turtlebg.appendChild(pic);
 }
-floatbomb(9);
 
 //make a goldentomb
 var floatgolden=function(k){
@@ -129,41 +208,55 @@ var floatgolden=function(k){
   pic.setAttribute("id","floatball"+k);
   pic.style.position="absolute";
   pic.style.left=Math.floor((Math.random()*92)+1)+"%";
+  pic.style.transform="rotate("+Math.floor((Math.random()*360))+"deg)";
   turtlebg.appendChild(pic);
 }
 
 //dropstone animation
-var dropstone=function(ball){
-  var timedrop=0;
-  var dropball=setInterval(function(){
-    timedrop=timedrop+20;
-    ball.style.bottom = 100-timedrop*0.001*33.3+"%";
-
-  },20);
+var dropstone=function(ball,turtle,i){
+  
+    balls[i][1]=balls[i][1]+16.7;
+    ball.style.bottom = 100-balls[i][1]*0.001*33.3+"%";
+    if(parseInt(turtle.style.left)-parseInt(ball.style.left)<8&&parseInt(turtle.style.left)-parseInt(ball.style.left)>(-growconstant)&&parseInt(ball.style.bottom)-parseInt(turtle.style.bottom)<growconstant*0.644&&parseInt(ball.style.bottom)-parseInt(turtle.style.bottom)>(-9.3)){
+      if(balls[i][0]=="black")
+        point=point+5;
+      else if(balls[i][0]=="golden")
+        point=point+10;
+      ball.remove();
+      turtleshocked.style.visibility="hidden";
+    }
+    else if(parseInt(ball.style.bottom)<-22)
+      ball.remove();
 } 
 
 //dropflower animation
-var dropflower=function(ball){
-  var timedrop=0;
-  var dropball=setInterval(function(){
-    timedrop=timedrop+20;
-    ball.style.bottom = 100-timedrop*0.001*20+"%";
-    ball.style.transform = "rotate(" + timedrop*4 + "deg)";
- },20);
+var dropflower=function(ball,turtle,i){
+ 
+    balls[i][1]=balls[i][1]+16.7;
+    ball.style.bottom = 100-balls[i][1]*0.001*20+"%";
+    ball.style.transform = "rotate(" + balls[i][1]*4 + "deg)";    
+    if(parseInt(turtle.style.left)-parseInt(ball.style.left)<7&parseInt(turtle.style.left)-parseInt(ball.style.left)>(-growconstant)&&parseInt(ball.style.bottom)-parseInt(turtle.style.bottom)<growconstant*0.644&&parseInt(ball.style.bottom)-parseInt(turtle.style.bottom)>(-7)){
+      ball.remove();
+      grow=1;
+    }
+    else if(parseInt(ball.style.bottom)<-22)
+      ball.remove();
 } 
 
 //dropbomb animation
-var dropbomb=function(ball,turtle){
-  var timedrop=0;
-  
-  function timebomb(){ 
-    timedrop=timedrop+16.7;
-    ball.style.bottom = 100-timedrop*0.001*40+"%";
-    ball.style.transform = "rotate(" + timedrop*0.2 + "deg)";
-    dropball=requestAnimationFrame(timebomb); 
-  }
-
-  var dropball=requestAnimationFrame(timebomb);
+var dropbomb=function(ball,turtle,i){
+   
+    balls[i][1]=balls[i][1]+16.7;
+    ball.style.bottom = 100-balls[i][1]*0.001*40+"%";
+    ball.style.transform = "rotate(" + balls[i][1]*0.2 + "deg)"; 
+    if(parseInt(turtle.style.left)-parseInt(ball.style.left)<8&&parseInt(turtle.style.left)-parseInt(ball.style.left)>(-growconstant)&&parseInt(ball.style.bottom)-parseInt(turtle.style.bottom)<growconstant*0.644&&parseInt(ball.style.bottom)-parseInt(turtle.style.bottom)>(-8)){
+      point=point-15;
+      ball.remove();
+      turtleshocked.style.visibility="visible";
+    }
+    else if(parseInt(ball.style.bottom)<-22)
+      ball.remove();
 } 
-dropbomb(document.getElementById("floatball9"),turtle);
-alert(parseInt(document.getElementById("floatball9").style.left)+parseInt(turtle.style.left));
+}//end if
+run=requestAnimationFrame(rungame);
+}//end
